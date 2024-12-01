5b0b131b60e871a65146eba53447e6be16cf8998
MahmoudMadboly <168362838+MahmoudMadboly@users.noreply.github.com>
2024-12-02 00:21:38 +0300

    code refactor

--- src/main/java/pages/AddAdminPage.java
+++ src/main/java/pages/AddAdminPage.java
@@ -15,11 +15,12 @@ public class AddAdminPage extends PageBase{
 		super(driver);
 	}
 	
-	By addAdminButton = By.xpath("//button[text()=' Add ']");
-	By userRoleMenu = By.xpath("//div [text() = '-- Select --'][1]");
-	//By userRole = By.xpath("//span [text() = 'Admin']");
-	By userRole = By.xpath("//div [class = 'oxd-select-option'][2]");
-	By employeeName = By.xpath("//input[@placeholder='Type for hints...']");
+	public static By systemUserTitle = By.xpath("//h5 [text() = 'System Users']");
+    protected static By addAdminButton = By.xpath("//button[text()=' Add ']");
+    By userRoleMenu = By.xpath("//div [text() = '-- Select --'][1]");
+    By userRole = By.xpath("//div [span[text()='Admin']]");
+    By employeeName = By.xpath("//input[@placeholder='Type for hints...']");
+    By employeeNameSuggestion = By.xpath("//div[@class='oxd-autocomplete-option']//span[text()='manda akhil user']");
 	By userName = By.xpath("//input [class = 'oxd-input oxd-input--active']");
 	By statusMenu = By.xpath("//div [text() = '-- Select --'][2]");
 	By status = By.xpath("//span [text() = 'Enabled']");
@@ -34,9 +35,9 @@ public class AddAdminPage extends PageBase{
 	}
 
 
-	public void setReuiredInfo (String employee_Name , String user_name , String pass_word , String confirm_password) {
+	public void setReuiredInfo (String employee_Name , String user_name , String pass_word , String confirm_password)throws InterruptedException  {
 
-		waitSomeTime(ExpectedConditions.presenceOfElementLocated(addUsertitle), 3);
+		/*waitSomeTime(ExpectedConditions.presenceOfElementLocated(addUsertitle), 3);
 		driver.findElement(employeeName).sendKeys(employee_Name);
 		driver.findElement(userRoleMenu).click();
 		waitSomeTime(ExpectedConditions.elementToBeClickable(userRole), 3);
@@ -46,7 +47,17 @@ public class AddAdminPage extends PageBase{
 		driver.findElement(status).click();
 		driver.findElement(userName).sendKeys(user_name);
 		driver.findElement(password).sendKeys(pass_word);
-		driver.findElement(confirmPassword).sendKeys(confirm_password);
+		driver.findElement(confirmPassword).sendKeys(confirm_password);*/
+		
+		 waitSomeTime(ExpectedConditions.visibilityOfElementLocated(addUsertitle), 3);
+	        driver.findElement(employeeName).sendKeys(employee_Name);
+	        waitSomeTime(ExpectedConditions.visibilityOfElementLocated(employeeNameSuggestion), 30);
+	        WebElement employeeNameSuggestionElement = driver.findElement(employeeNameSuggestion);
+	        employeeNameSuggestionElement.click();
+	        driver.findElement(userRoleMenu).click();
+	        waitSomeTime(ExpectedConditions.elementToBeClickable(userRole), 3);
+	        WebElement userRoleElement = driver.findElement(userRole);
+	        userRoleElement.click();
 	}
 
 	public void userClicksOnSaveButton() {
